aeb9e78b4f839ec9cf43196ec766b543b9d0e3c2
Sadman Khan <sadman.khan@coinbase.com>
2015-12-30 02:27:05 -0500

    updating tests for public client

--- tests/public_client.js
+++ tests/public_client.js
@@ -1,15 +1,26 @@
 var assert = require('assert');
+var nock = require('nock');
 
 var CoinbaseExchange = require('../index.js');
+var publicClient = new CoinbaseExchange.PublicClient();
 
 test('public client should return values', function(done) {
-  var publicClient = new CoinbaseExchange.PublicClient();
+  
+  nock('https://api.exchange.coinbase.com')
+  .get('/products/BTC-USD/ticker')
+  .reply(200, {
+    trade_id: 'test-id',
+    price: '9.00',
+    size: '5'
+  });
 
   publicClient.getProductTicker(function(err, resp, data) {
-    assert(data);
-    assert(data.trade_id);
-    assert(data.price);
-    assert(data.size);
+    assert.ifError(err);
+
+    assert.equal(data.trade_id, 'test-id');
+    assert(data.price, '9.00');
+    assert(data.size, '5');
+    
     done();
   });
 });
