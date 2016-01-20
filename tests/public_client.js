cb740165c714dbb24b925e7bf7c4abf9648485a5
sh6khan <sadman.khan999@gmail.com>
2016-01-20 11:27:59 -0500

    optional pagination for get product trades

--- tests/public_client.js
+++ tests/public_client.js
@@ -4,9 +4,39 @@ var nock = require('nock');
 var CoinbaseExchange = require('../index.js');
 var publicClient = new CoinbaseExchange.PublicClient();
 
+var EXCHANGE_API_URL = 'https://api.exchange.coinbase.com';
+
+test('get product trades', function(done) {
+  var expectedRepsonse = [{
+    "time": "2014-11-07T22:19:28.578544Z",
+    "trade_id": 74,
+    "price": "10.00000000",
+    "size": "0.01000000",
+    "side": "buy"
+  }, {
+    "time": "2014-11-07T01:08:43.642366Z",
+    "trade_id": 73,
+    "price": "100.00000000",
+    "size": "0.01000000",
+    "side": "sell"
+  }];
+
+  nock(EXCHANGE_API_URL)
+      .get('/products/BTC-USD/trades')
+      .reply(200, expectedRepsonse);
+
+  publicClient.getProductTrades(function(err, resp, data) {
+    assert.ifError(err);
+    assert.deepEqual(data, expectedRepsonse);
+
+    nock.cleanAll();
+    done();
+  });
+});
+
 test('public client should return values', function(done) {
   
-  nock('https://api.exchange.coinbase.com')
+  nock(EXCHANGE_API_URL)
   .get('/products/BTC-USD/ticker')
   .reply(200, {
     trade_id: 'test-id',
@@ -20,7 +50,8 @@ test('public client should return values', function(done) {
     assert.equal(data.trade_id, 'test-id');
     assert(data.price, '9.00');
     assert(data.size, '5');
-    
+  
+    nock.cleanAll();    
     done();
   });
 });
