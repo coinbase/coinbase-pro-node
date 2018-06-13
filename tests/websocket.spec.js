42b3ddec28e8b1b05cd0188a8264e4a1597d6159
Ryan McGeary <ryan@mcgeary.org>
2018-06-13 08:30:18 -0600

    Fix websocket test

--- tests/websocket.spec.js
+++ tests/websocket.spec.js
@@ -7,11 +7,10 @@ let port = 56632;
 suite('WebsocketClient', () => {
   test('connects to specified server', done => {
     const server = testserver(port, () => {
-      const websocketClient = new Gdax.WebsocketClient(
-        ['BTC-EUR'],
-        'ws://localhost:' + port
-      );
-      websocketClient.on('open', () => {
+      new Gdax.WebsocketClient(['BTC-EUR'], 'ws://localhost:' + port);
+    });
+    server.on('connection', socket => {
+      socket.on('message', () => {
         server.close();
         done();
       });
